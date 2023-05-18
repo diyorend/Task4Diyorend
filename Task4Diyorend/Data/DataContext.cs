9ca1fb005b94eaa97836a691ebf07e5bb4d324e2
Mukhammadislom <diyorend@gmail.com>
2023-05-18 12:04:29 +0500

    Add project files.

---
+++ Task4Diyorend/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Task4Diyorend.Models;
+
+namespace Task4Diyorend.Data
+{
+    public class DataContext : IdentityDbContext<AppUser>
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ Task4Diyorend/Models/AppUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Task4Diyorend.Models
+{
+    public class AppUser: IdentityUser
+    {
+        public DateTime RegistrationTime { get; set; } = DateTime.Now;
+        public DateTime LastLoginTime { get; set; } = DateTime.Now;
+        public string Name { get; set; }
+        public bool ActiveStatus { get; set; } = true;
+
+    }
+}
